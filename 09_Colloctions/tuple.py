d392d7a8aeb354171fcec2fd897d4472b500eb09
modukuriv <modukuri.vinodkumar26@gmail.com>
2025-12-08 21:30:58 -0500

    SingleTuple

--- 09_Colloctions/tuple.py
+++ 09_Colloctions/tuple.py
@@ -6,3 +6,12 @@ print(my_tuple)  # Output: (1, 2, 3, 'apple', 'banana', True, 3.14)
 my_tuple1 = (501, 502, 503, "Ramu", "Somu", "Krishan", 80.5, 90.5, 100.5)
 print(my_tuple1)  # Output: (501, 502, 503, 'Ramu', 'Somu', 'Krishan', 80.5, 90.5, 100.5) 
   
+
+tuple1 = ("Car",)
+print(tuple1)   # Output: ('Car')
+print(type(tuple1))  # Output: <class 'tuple'>
+
+tuple2=("Bike")
+print(tuple2)   # Output: Bike
+print(type(tuple2))  # Output: <class 'str'>
+# Note: To create a tuple with a single item, you need to include a comma after the item.
